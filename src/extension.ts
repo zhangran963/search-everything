8d360fb4e27ddda02ed542aca1a201c58afb593a
zhangran2 <zhangran2@wps.cn>
2024-01-18 03:44:32 -0500

    feat: 调试命令

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,26 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('extension.searchChineseCharacters', async () => {
-    const regex = "'[\\u4e00-\\u9fa5]+'"; // 注意转义字符
-    await vscode.commands.executeCommand('workbench.action.findInFiles', {
-      query: regex,
-      isRegex: true
+    const userInput = await vscode.window.showInputBox({
+      prompt: 'Enter a regular expression for searching Chinese characters:',
+      placeHolder: "'[\\u4e00-\\u9fa5]+'"
     });
+
+    const regex = userInput || "'[\\u4e00-\\u9fa5]+'"; // 使用用户输入或默认值
+
+    try {
+      await vscode.commands.executeCommand('workbench.action.findInFiles', {
+        query: regex,
+        isRegex: true
+      });
+      // 可以在此处添加代码以显示搜索结果概览或其他反馈
+    } catch (error: any) {
+      vscode.window.showErrorMessage('Search failed: ' + error.message);
+    }
   });
 
   context.subscriptions.push(disposable);
 }
-
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
